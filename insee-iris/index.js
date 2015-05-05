8b0374b0c007932f49d811c2001c8624f6927fec
Armand Gilles <arm.gilles@gmail.com>
2015-05-05 12:54:34 +0200

    Versin steam ok on 1 files, KO on > 2

--- insee-iris/index.js
+++ insee-iris/index.js
@@ -8,15 +8,30 @@ var Map = require('es6-map');
 var proj4 = require('proj4');
 
 
-// define projection
-var proj4 = require('proj4');
+// Metropolitan France
 proj4.defs["EPSG:2154"] = "+proj=lcc +lat_1=49 +lat_2=44 +lat_0=46.5 +lon_0=3 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs";
 var epsg2154 = proj4.defs['EPSG:2154'];
 var projector = proj4(epsg2154, proj4.WGS84);
 
 
-var output = fs.createWriteStream("data/output.geojson");
-output.write("[");
+// for Guadeloupe, Saint-Barthélemy, Saint-Martin, Martinique 
+proj4.defs["EPSG:32620"] = "+proj=utm +zone=20";
+var UTM20 = proj4.defs["EPSG:32620"];
+var projectorUTM20 = proj4(UTM20, proj4.WGS84);
+
+// for Guyane
+proj4.defs["EPSG:2972"] = "+proj=utm +zone=22";
+var UTM22 = proj4.defs["EPSG:2972"];
+var projectorUTM22 = proj4(UTM22, proj4.WGS84);
+
+// for Réunion
+proj4.defs["EPSG:2975"] = "+proj=utm +zone=40";
+var UTM40 = proj4.defs["EPSG:2975"];
+var projectorUTM40 = proj4(UTM40, proj4.WGS84);
+
+
+var output = fs.createWriteStream("data/iris.json");
+output.write('{ "type": "FeatureCollection", "features": [\n');
  
 
 // unzip files
@@ -37,7 +52,7 @@ unzipTask.extractFull('data/iris-france.7z', 'data/iris')
     console.log('Done extracting shapefiles: ', shapefiles.length);
 
     // convert shapefiles to geojson
-    shapefiles.forEach(function(file){
+    shapefiles.slice(0, 1).forEach(function(file){
 
       var reader = shapefile.reader(file)
       reader.readHeader(function(error, header) {
@@ -52,21 +67,43 @@ unzipTask.extractFull('data/iris-france.7z', 'data/iris')
             throw error;
           }
           if (record === shapefile.end) {
-            output.write("]");
+            output.write("]}");
             console.log("end of file : ", file);
             return reader.close();
           } else {
-
-              // convert coordinates  in WGS
+              // coordinates to convert
               var coord = record.geometry.coordinates[0];
+              
+              switch(record.properties.DEPCOM.substring(0, 3)) {
+                // for Guadeloupe, Saint-Barthélemy, Saint-Martin, Martinique 
+                case [971, 977, 978, 972]: // Guadeloupe, Saint-Barthélemy, Saint-Martin, Martinique
+                  var newCoord = coord.map(function(c) {
+                    return projectorUTM20.forward(c);
+                  });
+                  break;
 
-              var newCoord = coord.map(function(c) {
-                return projector.forward(c);
-              });
+                // for Guyane
+                case 973:
+                  var newCoord = coord.map(function(c) {
+                    return projectorUTM22.forward(c);
+                  });
+                  break;
+
+                // for Réunion
+                case 974:
+                  var newCoord = coord.map(function(c) {
+                    return projectorUTM40.forward(c);
+                  });
+                  break;
+
+                default:
+                  var newCoord = coord.map(function(c) {
+                    return projector.forward(c);
+                  });
+              }
             record.geometry.coordinates = [newCoord];
             output.write(JSON.stringify(record));
-            output.write(",");
-
+            output.write(",\n");
           };
           setImmediate(readNextRecord);
         });
