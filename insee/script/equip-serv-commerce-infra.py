a8204f2721fba9b9abb5ac60ce8c4ac043201c84
Armand Gilles <arm.gilles@gmail.com>
2015-05-15 10:15:14 +0200

    Adding enseignement supérieur

--- insee/script/equip-serv-commerce-infra.py
+++ insee/script/equip-serv-commerce-infra.py
@@ -17,7 +17,7 @@ features = [x for x in header if x not in ['CODGEO','LIBGEO','COM','LIBCOM','REG
 commerce['nb_commerce'] =  commerce[features].applymap(lambda x: float(x)).sum(axis=1)
 
 data = commerce
-print "il y a  %d iris différentes pour le commerce" % len(commerce.CODGEO.unique())
+print "il y a  %d iris différentes pour le commerce et %d features" % (len(commerce.CODGEO.unique()), len(features))
 
 
 ## Sport
@@ -31,7 +31,7 @@ sport = sport[5:]
 features = [x for x in header if x not in ['CODGEO','LIBGEO','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010','UU2010']]
 sport['nb_sport'] =  sport[features].applymap(lambda x: float(x)).sum(axis=1)
 [features.append(i) for i in ['nb_sport', 'CODGEO']]
-print "il y a  %d iris différentes pour le sport" % len(commerce.CODGEO.unique())
+print "il y a  %d iris différentes pour le sport et %d features" % (len(sport.CODGEO.unique()), len(features) - 1)
 
 data = pd.merge(data, sport[features], on='CODGEO', how='outer')
 
@@ -47,8 +47,40 @@ enseignement_1 = enseignement_1[5:]
 features = [x for x in header if x not in ['CODGEO','LIBGEO','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010','UU2010']]
 enseignement_1['nb_enseignement_1'] =  enseignement_1[features].applymap(lambda x: float(x)).sum(axis=1)
 [features.append(i) for i in ['nb_enseignement_1', 'CODGEO']]
-print "il y a  %d iris différentes pour l'enseignement du 1er degré" % len(enseignement_1.CODGEO.unique())
+print "il y a  %d iris différentes pour l'enseignement du 1er degré et %d features" % (len(enseignement_1.CODGEO.unique()), len(features) - 1)
 
 data = pd.merge(data, enseignement_1[features], on='CODGEO', how='outer')
+
+
+## Enseignement du second degré 
+enseignement_2 = pd.read_excel('../data/equip-serv-ens-2eme-degre-infra.xls', sheetname='IRIS')
+# creating header from file
+header = enseignement_2.loc[4].tolist()
+enseignement_2.columns = header
+# to get real values
+enseignement_2 = enseignement_2[5:]
+# creating new feature : sum of all feature
+features = [x for x in header if x not in ['CODGEO','LIBGEO','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010','UU2010']]
+enseignement_2['nb_enseignement_2'] =  enseignement_2[features].applymap(lambda x: float(x)).sum(axis=1)
+[features.append(i) for i in ['nb_enseignement_2', 'CODGEO']]
+print "il y a  %d iris différentes pour l'enseignement du second degré et %d features" % (len(enseignement_2.CODGEO.unique()), len(features) - 1)
+
+data = pd.merge(data, enseignement_2[features], on='CODGEO', how='outer')
+
+
+## Enseignement supérieur 
+enseignement_sup = pd.read_excel('../data/equip-serv-ens-sup-form-serv-infra.xls', sheetname='IRIS')
+# creating header from file
+header = enseignement_sup.loc[4].tolist()
+enseignement_sup.columns = header
+# to get real values
+enseignement_sup = enseignement_sup[5:]
+# creating new feature : sum of all feature
+features = [x for x in header if x not in ['CODGEO','LIBGEO','COM','LIBCOM','REG','DEP','ARR','CV','ZE2010','UU2010']]
+enseignement_sup['nb_enseignement_sup'] =  enseignement_sup[features].applymap(lambda x: float(x)).sum(axis=1)
+[features.append(i) for i in ['nb_enseignement_sup', 'CODGEO']]
+print "il y a  %d iris différentes pour l'enseignement du supérieur et %d features" % (len(enseignement_sup.CODGEO.unique()), len(features) - 1)
+
+data = pd.merge(data, enseignement_sup[features], on='CODGEO', how='outer')
 
 
