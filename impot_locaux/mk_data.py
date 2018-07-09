f19e95a1e5b969ca3ddb81fb7edb0e3e557e5095
Benoît Paris <benoit.paris.753@gmail.com>
2018-07-09 18:48:32 +0200

    Python 2 -> 3 (print ()), typo

--- impot_locaux/mk_data.py
+++ impot_locaux/mk_data.py
@@ -21,7 +21,7 @@ for file_path in import_files:
     small_year = file_year[2:]                  # 13
     
     # Loading file in DataFrame
-    print "Loading " + file_year + " REI's file (takes some time +/- 6 min)"
+    print("Loading " + file_year + " REI's file (takes some time +/- 6 min)")
     df_temp = pd.read_excel(file_path, sheetname='REI_'+file_year)
     # For merging after
     df_temp['IDCOM'] = df_temp['IDCOM'].astype('str') 
@@ -30,8 +30,8 @@ for file_path in import_files:
     all_columns = df_temp.columns
     
     REI_features = [col for col in all_columns if col not in geo_features]
-    print "REI " + file_year + " have " + str(len(REI_features)) + " features and "\
-    + str(len(df_temp)) + " lines" # 0.002 s / 0.0014
+    print("REI " + file_year + " have " + str(len(REI_features)) + " features and "\
+    + str(len(df_temp)) + " lines") # 0.002 s / 0.0014
     
     # Transform features name
     # B11 -> B11_13   
@@ -60,17 +60,5 @@ for file_path in import_files:
         # IDCOM change with year so we have to left-join to keep histo
         df = df.merge(df_temp[merge_feature], on="IDCOM", how='left')
     
-print "Export reslult in : data/impot_ouput.csv"    
+print("Export result in : data/impot_ouput.csv")
 df.to_csv('data/impot_ouput.csv', index=False, encoding='utf-8')
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
